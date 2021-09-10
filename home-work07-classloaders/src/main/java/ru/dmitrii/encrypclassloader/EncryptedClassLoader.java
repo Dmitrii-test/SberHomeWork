d6cab88fb8e93ed24811eb80f45298f0cd3b5fcf
gorbenko.dmitrii <gorbenko.dmitrii@otr.ru>
2021-09-10 10:55:15 +0300

    Правки задачи 2 темы 7

--- home-work07-classloaders/src/main/java/ru/dmitrii/encrypclassloader/EncryptedClassLoader.java
+++ home-work07-classloaders/src/main/java/ru/dmitrii/encrypclassloader/EncryptedClassLoader.java
@@ -1,14 +1,12 @@
 package ru.dmitrii.encrypclassloader;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
 
 public class EncryptedClassLoader extends ClassLoader {
-    private final String key;
+    private final int key;
     private final File dir;
 
-    public EncryptedClassLoader(String key, File dir, ClassLoader parent) {
+    public EncryptedClassLoader(int key, File dir, ClassLoader parent) {
         super(parent);
         this.key = key;
         this.dir = dir;
@@ -17,24 +15,45 @@ public class EncryptedClassLoader extends ClassLoader {
     @Override
     protected Class<?> findClass(String name) throws ClassNotFoundException {
         Class<?> result;
-        String pathname = dir + "/" + name + ".class";
-        System.out.println(pathname);
+        String pathname = dir + "/" + name + ".crp";
         File f = new File(pathname);
-        System.out.println(f.exists());
+        System.out.println("Файл найден - " + f.exists());
         byte[] classBytes = loadFileAsBytes(f);
         result = defineClass(name, classBytes, 0,classBytes.length);
+        System.out.println("Класс "+ result.getName() + " загружен.");
         return result;
     }
 
-    public static byte[] loadFileAsBytes(File file) throws ClassNotFoundException {
+    public byte[] loadFileAsBytes(File file) throws ClassNotFoundException {
         if (!file.exists()) throw new ClassNotFoundException("Файл не доступен");
         byte[] result = new byte[(int) file.length()];
         try (FileInputStream f = new FileInputStream(file)) {
-            f.read(result, 0, result.length);
+            int i=-1;
+            int j = 0;
+            while((i=f.read())!=-1){
+                result[j++] = (byte) (i+key);
+//                result[j++] = (byte) (i);
+            }
         } catch (IOException e) {
             System.out.println("Ошибка чтения файла " + e.getMessage());
         }
         return result;
+    }
+
+    public void cryptedClass (String name) {
+        String pathname = dir + "/" + name + ".class";
+        File out = new File(dir+"/" + name + ".crp");
+        File in = new File(pathname);
+        try (FileOutputStream fileOut = new FileOutputStream(out);
+             FileInputStream fileIn = new FileInputStream(in)) {
+            int i = -1;
+            while ((i = fileIn.read()) != -1) {
+                fileOut.write(i-key);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Класс " + name + " зашифрован");
     }
 
     @Override
--- home-work07-classloaders/src/main/java/ru/dmitrii/encrypclassloader/Main.java
+++ home-work07-classloaders/src/main/java/ru/dmitrii/encrypclassloader/Main.java
@@ -1,16 +1,27 @@
 package ru.dmitrii.encrypclassloader;
 
 import java.io.File;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         String path = "./home-work07-classloaders/Plugins";
         File dir = new File(path);
-        EncryptedClassLoader loader = new EncryptedClassLoader("123", dir, null);
+        EncryptedClassLoader loader = new EncryptedClassLoader(12, dir, null);
+        loader.cryptedClass("Test");
         try {
             Class<?> test = loader.findClass("Test");
-        } catch (ClassNotFoundException e) {
-            System.out.println("Ошибка получения "); e.printStackTrace();
+            Object o = test.getConstructor().newInstance();
+            Method method = test.getMethod("print", String.class );
+            method.invoke(o, "Привет");
+        } catch (ClassNotFoundException | NoSuchMethodException e) {
+            System.out.println("Ошибка получения "+ e.getMessage());
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            System.out.println("Ошибка запуска метода " + e.getMessage());
+        } catch (InstantiationException e) {
+            System.out.println("Ошибка создания объекта " + e.getMessage());
         }
 
     }
