1bdb0a072c795365ffdf57fad0b9fb017d2f8d7c
gorbenko.dmitrii <gorbenko.dmitrii@otr.ru>
2021-09-16 16:54:30 +0300

    Сделал задачу темы 9

--- home-work09-lambda/src/main/java/ru/dmitrii/streams/Main.java
+++ home-work09-lambda/src/main/java/ru/dmitrii/streams/Main.java
@@ -7,9 +7,13 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
         List<Person> someCollection =
-                Arrays.asList(new Person("Piter", 30), new Person("Bob", 20), new Person("Jon", 55));
-        Map m = Streams.of(someCollection)
-                .filter(p -> p.getAge() > 20)
+                Arrays.asList(new Person("Piter", 30),
+                        new Person("Bob", 20),
+                        new Person("Jon", 55),
+                        new Person("Tomy", 15));
+        Map<String, Person> m = Streams.of(someCollection)
+//                .filter(p -> p.getAge() > 20)
+                .filter(p -> p.getName().length()>3)
                 .transform(p -> new Person(p.getName(),p.getAge() + 30))
                 .toMap(p -> p.getName(), p -> p);
         m.forEach((k,v)-> System.out.println("Key: " + k + ",value: " + v));
--- home-work09-lambda/src/main/java/ru/dmitrii/streams/Streams.java
+++ home-work09-lambda/src/main/java/ru/dmitrii/streams/Streams.java
@@ -1,18 +1,18 @@
 package ru.dmitrii.streams;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.*;
 import java.util.function.Function;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
 public class Streams<T> {
     private List<T> list;
-    private Iterator<T> iterator;
-    private Predicate<? super T> pridicate;
-    private Function<? super T, ? extends T> mapper;
+    Queue<Method> methodQueue = new LinkedList<>();
+    Queue<Predicate<? super T>> predicateQueue = new LinkedList<>();
+    Queue<Function<? super T, ? extends T>> functionQueue = new LinkedList<>();
 
     public Streams(List<T> list) {
-        iterator = Spliterators.iterator(list.spliterator());
         this.list = list;
     }
 
@@ -21,16 +21,63 @@ public class Streams<T> {
     }
 
     public Streams<T> filter(Predicate<? super T> predicate) {
-        this.pridicate = predicate;
+        predicateQueue.add(predicate);
+        try {
+            Method method = Streams.class.getDeclaredMethod("filterImp", Predicate.class);
+            methodQueue.add(method);
+            System.out.println("Метод добавлен " + method.getName());
+        } catch (NoSuchMethodException e) {
+            System.out.println("Ошибка получения метода " + e.getMessage());
+        }
         return this;
     }
 
-    public Streams <T> transform(Function<? super T, ? extends T> mapper) {
-        this.mapper = mapper;
+    private Streams<T> filterImp(Predicate<? super T> predicate) {
+        List<T> result = new ArrayList<>();
+        for (T element : list) {
+            if (predicate.test(element)) {
+                result.add(element);
+            }
+        }
+        list = result;
         return this;
     }
 
-    public <R> Map <R, T>  toMap(Function<? super T, ? extends R> key, Function<? super T, ? extends T> value) {
-        return list.stream().filter(pridicate).map(mapper).collect(Collectors.toMap(key, value));
+    public Streams<T> transform(Function<? super T, ? extends T> mapper) {
+        functionQueue.add(mapper);
+        try {
+            Method method = this.getClass().getDeclaredMethod("transformImpl", Function.class);
+            methodQueue.add(method);
+            System.out.println("Метод добавлен " + method.getName());
+        } catch (NoSuchMethodException e) {
+            System.out.println("Ошибка получения метода " + e.getMessage());
+        }
+        return this;
+    }
+
+    private Streams<T> transformImpl(Function<? super T, ? extends T> mapper) {
+        List<T> result = new ArrayList<>();
+        for (T t : this.list) {
+            result.add(mapper.apply(t));
+        }
+        this.list = result;
+        return this;
+    }
+
+    public <R> Map<R, T> toMap(Function<? super T, ? extends R> key, Function<? super T, ? extends T> value) {
+        while (!methodQueue.isEmpty()) {
+            Method method = methodQueue.poll();
+            try {
+                if (method.getName().equals("filterImp")) method.invoke(this, predicateQueue.poll());
+                else if (method.getName().equals("transformImpl")) method.invoke(this, functionQueue.poll());
+            } catch (IllegalAccessException | InvocationTargetException e) {
+                System.out.println("Ошибка запуска метода " + e.getMessage());
+            }
+        }
+        Map<R, T> map = new HashMap<>();
+        for (T element : list) {
+            map.put(key.apply(element), value.apply(element));
+        }
+        return map;
     }
 }
