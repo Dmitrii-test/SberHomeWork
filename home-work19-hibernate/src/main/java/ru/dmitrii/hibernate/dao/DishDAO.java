907ecbce04371d2292984b8fca3dc35e44fae8d2
gorbenko.dmitrii <gorbenko.dmitrii@otr.ru>
2021-10-22 18:31:07 +0300

    Правки к задаче темы 19

--- home-work19-hibernate/src/main/java/ru/dmitrii/hibernate/dao/DishDAO.java
+++ home-work19-hibernate/src/main/java/ru/dmitrii/hibernate/dao/DishDAO.java
@@ -5,42 +5,61 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.springframework.stereotype.Service;
 import ru.dmitrii.hibernate.model.Dish;
-import ru.dmitrii.hibernate.model.Recipe;
+
+import javax.persistence.*;
 
 @Service
 public class DishDAO {
 
-    final SessionFactory sessionFactory;
+//    final SessionFactory sessionFactory;
+
+    private EntityManager entityManager;
 
     public DishDAO() {
-        this.sessionFactory = HibernateSessionFactoryUtil.getSessionFactory();
+        EntityManagerFactory myUnit = Persistence.createEntityManagerFactory("myUnit");
+        entityManager = myUnit.createEntityManager();
+//        this.sessionFactory = HibernateSessionFactoryUtil.getSessionFactory();
     }
 
     public Dish findById(int id) {
-        return sessionFactory.openSession().get(Dish.class, id);
+        Dish dish = entityManager.find(Dish.class, id);
+        entityManager.detach(dish);
+        return dish;
+//     return sessionFactory.openSession().get(Dish.class, id);
     }
+
     public void save(Dish dish) {
-        Session session = sessionFactory.openSession();
-        Transaction tx1 = session.beginTransaction();
-        session.save(dish);
-        tx1.commit();
-        session.close();
+        entityManager.getTransaction().begin();
+        entityManager.persist(dish);
+        entityManager.getTransaction().commit();
+//        Session session = sessionFactory.openSession();
+//        Transaction tx1 = session.beginTransaction();
+//        session.save(dish);
+//        tx1.commit();
+//        session.close();
     }
 
     public void update(Dish dish) {
-        Session session = sessionFactory.openSession();
-        Transaction tx1 = session.beginTransaction();
-        session.update(dish);
-        tx1.commit();
-        session.close();
+        entityManager.getTransaction().begin();
+        entityManager.merge(dish);
+        entityManager.getTransaction().commit();
+//        Session session = sessionFactory.openSession();
+//        Transaction tx1 = session.beginTransaction();
+//        session.update(dish);
+//        tx1.commit();
+//        session.close();
     }
 
     public void delete(Dish dish) {
-        Session session = sessionFactory.openSession();
-        Transaction tx1 = session.beginTransaction();
-        session.delete(dish);
-        tx1.commit();
-        session.close();
+        entityManager.getTransaction().begin();
+        entityManager.remove(dish);
+        entityManager.getTransaction().commit();
+
+//        Session session = sessionFactory.openSession();
+//        Transaction tx1 = session.beginTransaction();
+//        session.delete(dish);
+//        tx1.commit();
+//        session.close();
     }
 
 //
